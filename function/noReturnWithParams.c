5da0d999d50848facd8d393bf9ff2dd65c10cde7
Mashrafi Bin Nur <mbngms@gmail.com>
2024-11-06 01:48:09 +0600

    void

--- function/noReturnWithParams.c
+++ function/noReturnWithParams.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int add(int a, int b) {
+void add(int a, int b) {
     int sum = a + b;
-    return sum;
+    
+    printf("%d\n", sum);
 }
 
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
     
-    int result = add(a, b);
-    
-    printf("%d\n", result);
+    add(a, b);
     
     return 0;
 }
