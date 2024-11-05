176a1475e013e001284593f13b38711591e3a19f
Mashrafi Bin Nur <mbngms@gmail.com>
2024-11-06 00:17:24 +0600

    return with no params

---
+++ function/returnNoParams.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    int sum = a + b;
+    
+    return sum;
+}
+
+int main() {
+    printf("%d", add());
+    
+    return 0;
+}
