9dd6422982c54a65a6bab5da938fa09a49fb3fe2
Mashrafi Bin Nur <mbngms@gmail.com>
2024-11-09 16:35:04 +0600

    limit one to n recursion

--- recursion/oneToN.c
+++ recursion/oneToN.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void limit(int x, int n) {
+
+    if(x > n) {
+        return;
+    }
+
+    printf("%d\n", x);
+    limit(x + 1, n);
+}
+
 int main() {
-    
+    int n;
+    scanf("%d", &n);
+
+    limit(1, n);
 }
