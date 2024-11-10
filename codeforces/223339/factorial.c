470c994ce8f086ca70dd028cecb13bcfefd1f3a6
Mashrafi Bin Nur <mbngms@gmail.com>
2024-11-11 00:40:14 +0600

    factorial

---
+++ codeforces/223339/factorial.c
@@ -0,0 +1,22 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/J
+
+#include <stdio.h>
+
+long long factorial(int n) {
+    if(n == 0) {
+        return 1;
+    }
+    
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    long long result = factorial(n);
+    
+    printf("%lld\n", result);
+    
+    return 0;
+}
